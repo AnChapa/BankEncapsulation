2219b4e126e2ede247db1997773b2c3b0bc84ab7
Antonio Chapa <antonio.luis.chapa@gmail.com>
2024-01-25 11:34:17 -0600

    initial commit

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double balance = 0;
+
+        public void Deposit(double money)
+        {
+            balance += money;
+        }
+
+        public double GetBalance()
+        {
+            return balance;
+        }
+    }
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BankAccount account = new BankAccount();
+            string doneString = "";
+            double deposit = 0;
+            while (doneString != "done")
+            {
+                Console.WriteLine("What would you like to do today?\nWe can Deposit or Check your balance for you.\nIf you are finished, type Done.");
+                doneString = Console.ReadLine().ToLower();
+                Console.WriteLine("---------------------------------");
+                switch (doneString)
+                {
+                    case "deposit":
+                        Console.WriteLine("How much would you like to deposit:");
+                        account.Deposit(deposit = double.Parse(Console.ReadLine()));
+                        Console.WriteLine($"We have deposited the ${deposit} into your account.");
+                        break;
+                    case "check":
+                        Console.WriteLine($"You have ${account.GetBalance()} in your account.");
+                        break;
+                    case "done":
+                        break;
+                    default:
+                        Console.WriteLine("We are sorry, but this is not a valid command.");
+                        break;
+                }
+                Console.WriteLine("---------------------------------");
+            }
+            Console.WriteLine("Thank you for your time. We hope to serve you again.");
         }
     }
 }
